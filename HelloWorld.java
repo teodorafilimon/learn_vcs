da39dd3606f228c55f0f0d3d078dfd82b17a617b
Teodora Filimon <teodora.filimon@yahoo.ro>
2019-11-06 11:14:53 -0800

    Modify text

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void  main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Testing a new version");
+        System.out.println("Testing group collaboration");
 } 
 }
